028b5366ffd58a23137fb7e02da2f6da0476cd1a
daziel17 <edeivid.1704@gmail.com>
2022-01-18 07:49:47 -0500

    feat: get image static for waypoints

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pcMap = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pcMap)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(12, 48);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(767, 20);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pcMap
+            // 
+            this.pcMap.Location = new System.Drawing.Point(244, 74);
+            this.pcMap.Name = "pcMap";
+            this.pcMap.Size = new System.Drawing.Size(603, 487);
+            this.pcMap.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcMap.TabIndex = 2;
+            this.pcMap.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 594);
+            this.Controls.Add(this.pcMap);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtUrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pcMap)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pcMap;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        string google_key = "";
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] waypoints = {
+                            "44.8765065,-0.4444849",
+                            "44.8843778,-0.1368667"
+            };
+            this.txtUrl.Text = this.getStaticMapURLForDirection("45.291002,-0.868131", "44.683159,-0.405704", waypoints);
+
+            var task = Task.Run(() => this.requestsImage(this.txtUrl.Text));
+            task.Wait();
+
+            this.pcMap.Image = Image.FromStream(task.Result);
+
+        }
+
+        public string getStaticMapURLForDirection(string origin, string destination, string[] waypoints, string size = "500x500") {
+
+            string[] markers = new string[waypoints.Length + 2];
+            int waypoints_label_iter = 0;
+
+            markers[waypoints_label_iter++] = this.formatMarkers("green", "_" + waypoints_label_iter, origin);
+            foreach (string waypoint in waypoints)
+            {
+                markers[waypoints_label_iter++] = this.formatMarkers("blue", "_" + waypoints_label_iter, waypoint);
+            }
+            markers[waypoints_label_iter++] = this.formatMarkers("red", "_" + waypoints_label_iter, destination);
+
+            string url = string.Format("https://maps.googleapis.com/maps/api/directions/json?origin={0}&destination={1}&waypoints={2}&key={3}", 
+                                        origin,
+                                        destination, 
+                                        string.Join("|", waypoints), this.google_key);
+
+            var task = Task.Run(()=> this.requestsAsync(url));
+            task.Wait();
+            JObject googleDirection = JObject.Parse(task.Result);
+
+            var a = googleDirection["routes"][0];
+
+            string polyline = HttpUtility.UrlEncode(googleDirection["routes"][0]["overview_polyline"]["points"].ToString());
+            return string.Format("https://maps.googleapis.com/maps/api/staticmap?size={0}&maptype=roadmap&path=enc:{1}&{2}&key={3}", size, polyline, string.Join("&", markers), this.google_key);
+        }
+
+        private string formatMarkers( string color, string label, string waypoint) {
+            string formatMarker = "markers=color:{0}{1}label:{2}";
+            label = HttpUtility.UrlEncode(string.Format("{0}|{1}", label, waypoint));
+            return string.Format(formatMarker, color, HttpUtility.UrlEncode("|"), label);
+        }
+
+        private async Task<string> requestsAsync(string url) {
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                var content = await response.Content.ReadAsStringAsync();
+                return content;
+            }
+        }
+
+        private async Task<System.IO.Stream> requestsImage(string url)
+        {
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                var content = await response.Content.ReadAsStreamAsync();
+                return content;
+            }
+        }
+
+    }
+}
